6b48172a029dd9ff7eb0f14a2ed43146d7d9e252
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2020-12-18 17:54:09 +0100

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import { AppContainer } from "./styles";
+import Card from "./components/Card";
+import Column from "./components/Column";
+import AddNewItem from "./components/AddNewItem";
 
 const App = () => {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate App Scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 };
 
 export default App;
