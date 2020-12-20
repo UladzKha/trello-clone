794ba37f35d38984afb909ae5f02b00089a40bcc
Uladzimir Khadakouski <uladzimir.khadakouski@intive.com>
2020-12-20 18:29:40 +0100

    feat: autofocus on input

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "../styles";
+import { useFocus } from "../utils/useFocus";
 
 interface NewItemFormProps {
   onAdd(text: string): void;
 }
 
-const NewItemForm = (props: NewItemFormProps) => {
-  const { onAdd } = props;
+const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
